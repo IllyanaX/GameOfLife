5756227b44520e61fafd409666cf059f3e036476
IllyanaX <teabird.x@gmail.com>
2023-08-01 20:52:41 -0600

    Finalización del código para generar el Juego de la Vida

--- gameoflife.py
+++ gameoflife.py
@@ -7,14 +7,14 @@ color_cuadricula = (38, 28, 44)
 celulapormorir = (180, 165, 165)
 celulaporvivir = (196, 182, 182)
 
-def update(pantalla, celulas, size, progreso = False):
-    update_celulas = np.zeros((celulas.shape[0],celulas.shape[1]))
+def update(pantalla, celulas, size, progreso=False):
+    update_celulas = np.zeros((celulas.shape[0], celulas.shape[1]))
     #El siguiente bucle permite tomar cada célula para ponerlas en la cuadrícula 
     #Tomando tanto la lógica del juego como la sintaxis de Python
     for fila, col in np.ndindex(celulas.shape):
         #Calcula cuantas celulas vivas hay alrededor de una célula cualquiera usando listas
         #La idea es sumar las celulas de arriba más las de abajo, restando la celula cuyo estado queremos definir.
-        celulaviva = np.sum(celulas[fila - 1 : fila + 2, col - 1 : col +2]) - celulas[fila, col]
+        celulaviva = np.sum(celulas[fila-1:fila+2, col-1:col+2]) - celulas[fila, col]
         color = color_fondo if celulas[fila, col] == 0 else celulaporvivir
 
         #El siguiente ciclo es para establecer las reglas de Juego de la vida
@@ -61,17 +61,17 @@ def main():
                 if event.key == pygame.K_SPACE:
                     running = not running
                     update(pantalla, celulas, 10)
-                    pygame.display.update
-                if pygame.mouse.get.pressed()[0]:
-                    pos = pygame.mouse.get_pos()
-                    celulas[pos[1] // 10, pos[0] // 10] = 1
+                    pygame.display.update()
+                if pygame.mouse.get_pressed()[0]:
+                    posicion = pygame.mouse.get_pos()
+                    celulas[posicion[1] // 10, posicion[0] // 10] = 1
                     update(pantalla, celulas, 10)
                     pygame.display.update()
 
         pantalla.fill(color_cuadricula)
 
         if running:
-            celulas = update(pantalla, celulas, 10, progreso = True)
+            celulas = update(pantalla, celulas, 10, progreso=True)
             pygame.display.update
         
         time.sleep(0.001)
